19794e78ce140b7a1e9afa041278d6919a79ef3a
Sebastián Reyes <sebastianreyes01@hotmail.com>
2018-11-11 22:32:31 -0500

    Info displayed.

--- src/main/resources/static/js/stockMarket.js
+++ src/main/resources/static/js/stockMarket.js
@@ -7,15 +7,34 @@ var StockMarket = (function(){
     var getInfo = function () {
         var func = document.getElementsByClassName("active")[0].id;
         var symbol = document.getElementById("inp").value;
-        instance.get('/' + func + '/' + symbol)
-            .then(function (response) {
-                // handle success
-                console.log(response);
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            });
+        if(func === "intraday") {
+            instance.get('/' + func + '/' + symbol + '/' + document.getElementById("select").value)
+                .then(function (response) {
+                    var div = document.getElementById("info");
+                    var p = document.createElement("p");
+                    var pText = document.createTextNode(response.data);
+                    p.appendChild(pText);
+                    div.appendChild(p);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+        } else {
+            instance.get('/' + func + '/' + symbol)
+                .then(function (response) {
+                    var div = document.getElementById("info");
+                    var p = document.createElement("p");
+                    var pText = document.createTextNode(response.data);
+                    p.appendChild(pText);
+                    div.appendChild(p);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+        }
+
     };
 
     return {
